7f5ea0448715bb4d4fc34e5e2084360bea23e6e1
Yaroslav Zghoba <yaroslavzghoba@gmail.com>
2024-11-22 22:10:27 +0100

    Added foundation for Android and IOS apps

---
+++ composeApp/src/commonMain/kotlin/com/yaroslavzghoba/papagei/App.kt
@@ -0,0 +1,8 @@
+package com.yaroslavzghoba.papagei
+
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App(): Unit = TODO("Not implemented yet")
---
+++ composeApp/src/iosMain/kotlin/com/yaroslavzghoba/papagei/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.yaroslavzghoba.papagei
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
